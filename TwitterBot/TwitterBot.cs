b0559b5b3161431b7b7fa5bbbce3389ee977189d
Ayaka Neko <neko@ayaka.moe>
2017-09-01 02:05:52 -0600

    Bot Summary

---
+++ TwitterBot/TwitterBot.cs
@@ -0,0 +1,51 @@
+﻿using moeInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterBot
+{
+    public class TwitterBot : IPlugin
+    {
+        public string Name
+        {
+            get
+            {
+
+            }
+        }
+
+        public string shortID
+        {
+            get
+            {
+
+            }
+        }
+
+        public string compatible_version
+        {
+            get
+            {
+
+            }
+        }
+
+        public string Hello()
+        {
+            
+        }
+
+        public void Start()
+        {
+            
+        }
+    }
+
+    class Twitter
+    {
+
+    }
+}
