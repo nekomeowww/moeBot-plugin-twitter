6d3b26c75b2dded18e725050c7ca56c2f08647f6
Ayaka Neko <neko@ayaka.moe>
2017-09-03 06:15:03 -0600

    File Ultilities Added

---
+++ TwitterBot/modules/FileUltilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace moeBot_plugin_twitter.modules
+{
+    public class FileUltilities
+    {
+        public bool FileExsit(string path)
+        {
+            bool result = File.Exists(path);
+            return result;
+        }
+    }
+}
